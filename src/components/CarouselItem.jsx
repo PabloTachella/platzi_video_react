1659bd65cb427f173e5a8df08d667ca3d9a72def
pabloxw <pablotachella95@gmail.com>
2021-06-19 15:57:02 -0300

    Se agregaron las imagenes al proyecto y se configuro file-loader para manipularlas en el build

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import '../assets/styles/components/CarouselItem.scss'
 
+import iconPlay from '../assets/static/icon-play.png'
+import iconPlus from '../assets/static/icon-plus.png'
+
 const CarouselItem = () => (
     <div className="carousel-item">
         <img className="carousel-item__img" src="https://source.unsplash.com/random?movie" alt=""/>
         <div className="carousel-item__details">
             <div>
-                <img src="images/icon-play.png" alt="play" />
-                <img src="images/icon-plus.png" alt="plus" />
+                <img src={iconPlay} alt="play" />
+                <img src={iconPlus} alt="plus" />
             </div>
             <p className="carousel-item__details--title">Título descriptivo</p>
             <p className="carousel-item__details--subtitle">2021 16+ 120 minutos</p>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import '../assets/styles/components/Header.scss'
 
+import logo from '../assets/static/logo-platzi-video.png'
+import userIcon from '../assets/static/user-icon.png'
+
 const Header = () => (
     <header className="header">
-        <img className= "header__img" src="images/logo-platzi-video.png" alt="Logo Platzi logo-platzi-video"/>
+        <img className= "header__img" src={logo} alt="Logo Platzi logo-platzi-video"/>
         <div className="header__menu">
             <div className="header__menu--profile">
-                <img src="images/user-icon.png" alt="User icon"/>
+                <img src={userIcon} alt="User icon"/>
                 <p>Perfil</p>
             </div>
             <ul>
