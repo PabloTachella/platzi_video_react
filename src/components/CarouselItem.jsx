004acb734e45c36e159fc60b34c2f221cc3e0327
pabloxw <pablotachella95@gmail.com>
2021-06-20 00:35:18 -0300

    Se incorporaron hooks y el uso de una FakeAPI para simular el comportamiento del renderizado mediante una peticion que consume dicha FakeAPI

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -4,16 +4,18 @@ import '../assets/styles/components/CarouselItem.scss'
 import iconPlay from '../assets/static/icon-play.png'
 import iconPlus from '../assets/static/icon-plus.png'
 
-const CarouselItem = () => (
+const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
     <div className="carousel-item">
-        <img className="carousel-item__img" src="https://source.unsplash.com/random?movie" alt=""/>
+        <img className="carousel-item__img" src={cover} alt={title} />
         <div className="carousel-item__details">
             <div>
                 <img src={iconPlay} alt="play" />
                 <img src={iconPlus} alt="plus" />
             </div>
-            <p className="carousel-item__details--title">Título descriptivo</p>
-            <p className="carousel-item__details--subtitle">2021 16+ 120 minutos</p>
+            <p className="carousel-item__details--title">{title}</p>
+            <p className="carousel-item__details--subtitle">
+            {`${year} ${contentRating} ${duration} Minutos`}    
+            </p>
         </div>
     </div>
 )
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from '../components/Header'
 import Search from '../components/Search'
 import Categories from '../components/Catergories'
@@ -8,52 +8,47 @@ import Footer from '../components/Footer'
 
 import '../assets/styles/App.scss'
 
-const App = () => (
-    <div className="App">
-        <Header />
-        <Search />
+const App = () => {
+    const [ videos, setVideos ] = useState([])
 
-        <Categories title="Mi Lista">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+    useEffect(() => {
+        fetch('http://localhost:3000/initialState')
+            .then(response => response.json())
+            .then(data => setVideos(data))
+    }, [] )
 
-        <Categories title="Tendencias">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+    console.log(videos)
 
-        <Categories title="Originales de Platzi Video">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+    return(
+        <div className="App">
+            <Header />
+            <Search />
 
-        <Footer />
-    </div>
-)
+            {videos.mylist?.lenght > 0 &&  //solo si mi lista es mayor a 0 renderiza esta seccion
+                <Categories title="Mi Lista">
+                    <Carousel>
+                        <CarouselItem />
+                    </Carousel>
+                </Categories>
+            }
+
+            <Categories title="Tendencias">
+                <Carousel>
+                    {videos.trends?.map(item =>
+                        <CarouselItem key={item.id} {...item} /> //al renderizado de carouselItem le paso el id del item y luego todos elementos que tiene item
+                    )}
+                </Carousel>
+            </Categories>
+
+            <Categories title="Originales de Platzi Video">
+                <Carousel>
+                    <CarouselItem />
+                </Carousel>
+            </Categories>
+
+            <Footer />
+        </div>
+    )
+}
 
 export default App
