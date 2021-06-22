f6e3620627abd0bd5c180ff3d2714941c0397ef8
Pablo <pablotachella95@gmail.com>
2021-06-22 19:00:19 -0300

    Se agrego el componente Layout que maneja la persistencia del header y footer en las diferentes rutas, teniendo a los demas componentes como hijos

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import Header from '../components/Header'
 import Search from '../components/Search'
 import Categories from '../components/Catergories'
 import Carousel from '../components/Carousel'
 import CarouselItem from '../components/CarouselItem'
-import Footer from '../components/Footer'
 import useInitialState from '../hooks/useInitialState'
 
 import '../assets/styles/App.scss'
@@ -17,8 +15,7 @@ const Home = () => {
     console.log(initialState)
 
     return initialState.length === 0 ? <h1>...Loading</h1> : (
-        <div className="App">
-            <Header />
+        <> 
             <Search />
 
             {initialState.mylist.lenght > 0 &&  //solo si mi lista es mayor a 0 renderiza esta seccion
@@ -46,9 +43,7 @@ const Home = () => {
                         )}
                 </Carousel>
             </Categories>
-
-            <Footer />
-        </div>
+        </>
     )
 }
 
