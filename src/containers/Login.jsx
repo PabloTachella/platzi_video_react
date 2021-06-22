9b890995488251b250afb4232102bd645d6aac2b
pabloxw <pablotachella95@gmail.com>
2021-06-21 21:39:28 -0300

    Se agrego el container o pagina Login y se le asigno una ruta en el archivo de rutas. Se configuro webpack.config.js para poder manejarnos con rutas

---
+++ src/containers/Login.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import googleIcon from '../assets/static/icon-google-plus.png'
+import twitterIcon from '../assets/static/icon-twitter.png'
+import '../assets/styles/components/LoginRegister.scss'
+
+const Login = () => (
+    
+    <section className="login sesion__main">
+        <section className="login__container">
+            <h2>Inicia Sesión</h2>
+            <form className="login__container--form">
+                <input className="sesion__input" type="email" name="" placeholder="Correo" />
+                <input className="sesion__input" type="password" name="" placeholder="Contraseña" />
+                <button className="button">Iniciar Sesión</button>
+                <div className="login__container--remember-me">
+                    <label>
+                        <input type="checkbox" name="" id="cbox1" />Recuérdame
+                    </label>
+                    <a href="/">Olvidé mi Contraseña</a>
+                </div>
+            </form>
+            <section className="login__container--social-media">
+                <div><img src={googleIcon} alt="Google" />Inicia sesión con Google</div>
+                <div><img src={twitterIcon} alt="Twitter" />Inicia sesión con Twitter</div>
+            </section>
+            <p className="login__container--register">No tienes ninguna cuenta <a href="/">Regístrate</a></p>
+        </section>
+    </section>
+)
+
+export default Login
