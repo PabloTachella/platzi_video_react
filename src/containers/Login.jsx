e59dbe84dba8b3cd0a373cf7ecd780ba4d124b3a
Pablo <pablotachella95@gmail.com>
2021-06-24 17:23:59 -0300

    Se agrego la logica para obtener y administrar los imputs del usuario en el formulario de inicio de sesion

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,35 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import googleIcon from '../assets/static/icon-google-plus.png'
 import twitterIcon from '../assets/static/icon-twitter.png'
 import '../assets/styles/components/LoginRegister.scss'
 
-const Login = () => (
-    
-    <section className="login sesion__main">
-        <section className="login__container">
-            <h2>Inicia Sesión</h2>
-            <form className="login__container--form">
-                <input className="sesion__input" type="email" name="" placeholder="Correo" />
-                <input className="sesion__input" type="password" name="" placeholder="Contraseña" />
-                <button className="button">Iniciar Sesión</button>
-                <div className="login__container--remember-me">
-                    <label>
-                        <input type="checkbox" name="" id="cbox1" />Recuérdame
-                    </label>
-                    <a href="/">Olvidé mi Contraseña</a>
-                </div>
-            </form>
-            <section className="login__container--social-media">
-                <div><img src={googleIcon} alt="Google" />Inicia sesión con Google</div>
-                <div><img src={twitterIcon} alt="Twitter" />Inicia sesión con Twitter</div>
+const Login = () => {
+    const [form, setValues] = useState({
+        email:'',
+    })
+
+    const handleInput = event => {
+        setValues({
+            ...form,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const handleSubmit = event => {
+        //preventDefault evita el comportamiento por defecto en los imput 
+        //como por ejemplo enviar parametros por url al presionar enter
+        event.preventDefault()
+        console.log(form)
+    }
+
+    return (
+        <section className="login sesion__main">
+            <section className="login__container">
+                <h2>Inicia Sesión</h2>
+                <form className="login__container--form" onSubmit={handleSubmit}>
+                    <input
+                        className="sesion__input"
+                        type="email"
+                        name="email"
+                        placeholder="Correo"
+                        onChange={handleInput}
+                    />
+                    <input
+                        className="sesion__input"
+                        type="password"
+                        name="password"
+                        placeholder="Contraseña" 
+                        onChange={handleInput}
+                    />
+                    <button className="button">Iniciar Sesión</button>
+                    <div className="login__container--remember-me">
+                        <label>
+                            <input type="checkbox" name="" id="cbox1" />Recuérdame
+                        </label>
+                        <a href="/">Olvidé mi Contraseña</a>
+                    </div>
+                </form>
+                <section className="login__container--social-media">
+                    <div><img src={googleIcon} alt="Google" />Inicia sesión con Google</div>
+                    <div><img src={twitterIcon} alt="Twitter" />Inicia sesión con Twitter</div>
+                </section>
+                <p className="login__container--register">No tienes ninguna cuenta 
+                <Link to="/register">
+                    Regístrate
+                </Link></p>
             </section>
-            <p className="login__container--register">No tienes ninguna cuenta 
-            <Link to="/register">
-                Regístrate
-            </Link></p>
         </section>
-    </section>
-)
+    )
+}
 
 export default Login
