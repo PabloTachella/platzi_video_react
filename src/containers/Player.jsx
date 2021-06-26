82e0bfc2933d55c52c502d6e87cea4125a9adf8a
Pablo <pablotachella95@gmail.com>
2021-06-25 22:46:01 -0300

    Se agrego logica al boton regresar de player y se agrego una configuracion a webpack para asignar el public path

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import '../assets/styles/components/Player.scss'
 
-const Player = () => {
+const Player = props => {
+    // props.match.params es lo que recibe react router en la ruta asignada a player
+    const { id } = props.match.params
     return (
         <div className="player">
             <video controls autoPlay>
                 <source src="" type="video/mp4" />
             </video>
             <div className="player-back">
-                <button type="button">
+                <button type="button" onClick={() => props.history.goBack()}>
                     Regresar
                 </button>
             </div>
