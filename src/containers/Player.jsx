0f8f86eef4e0a2a657266aae6f03c4d9e5600526
Pablo <pablotachella95@gmail.com>
2021-06-26 19:05:28 -0300

    Se configuro el initialState y player para que reproduzca videos

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
+import { getVideoSource } from '../actions'
 import '../assets/styles/components/Player.scss'
 
 const Player = props => {
     // props.match.params es lo que recibe react router en la ruta asignada a player
     const { id } = props.match.params
-    return (
+    const [loading, setLoading] = useState(true)
+    // Object.keys().length me permite saber cuantos elementos tiene un objeto
+    const hasPlaying = Object.keys(props.playing).length > 0
+    useEffect(() => {
+        props.getVideoSource(id)
+        setLoading(false)
+    }, [])
+    if (loading) {
+        return <h2>Cargando...</h2>;
+      }
+    return hasPlaying ? (
         <div className="player">
             <video controls autoPlay>
-                <source src="" type="video/mp4" />
+                <source src={props.playing.source} type="video/mp4" />
             </video>
             <div className="player-back">
                 <button type="button" onClick={() => props.history.goBack()}>
@@ -15,7 +28,17 @@ const Player = props => {
                 </button>
             </div>
         </div>
-    )
+    ) : <Redirect to="/404/" />
 }
 
-export default Player
+const mapStateToProps = state => {
+    return {
+        playing: state.playing,
+    }
+}
+
+const mapDispatchToProps = {
+    getVideoSource,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Player)
